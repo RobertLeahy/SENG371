582518b50a5b693b94638e9254f066d76f6b91f4
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2015-02-04 15:02:06 -0800

    Lab 4 - gitstats Refactor/File Support
    Refactored the gitstats.py script for better style/modularity.
    
    This allowed me to easily implement support for reading from a file, rather than from stdin.

--- Lab4/gitstats.py
+++ Lab4/gitstats.py
@@ -2,37 +2,44 @@ import re
 import sys
 
 
-def get_count (regex, str):
-	m=regex.search(line)
-	if not m:
-		return 0
-	return int(m.group(1))
+class GitStats:
+
+	def __get_count (self, regex, str):
+		m=regex.search(str)
+		if not m:
+			return 0
+		return int(m.group(1))
+	
+	def __init__ (self, file):
+		self.commits=0
+		self.insertions=0
+		self.deletions=0
+		self.last=""
+		cregex=re.compile("^commit ([a-f0-9]+)")
+		regex=re.compile("^ [0-9]")
+		iregex=re.compile("([0-9]+) insert")
+		dregex=re.compile("([0-9]+) delet")
+		last=""
+		for line in file:
+			c=cregex.match(line)
+			if c:
+				self.commits+=1
+				self.last=c.group(1)
+			if regex.match(line):
+				i=self.__get_count(iregex,line)
+				self.insertions+=i
+				d=self.__get_count(dregex,line)
+				self.deletions+=d
 
 
-display_sha=True
+if len(sys.argv)>1:
+	with open(sys.argv[1]) as f:
+		stats=GitStats(f)
+else:
+	stats=GitStats(sys.stdin)
 
 
-commits=0
-insertions=0
-deletions=0
-
-cregex=re.compile("^commit ([a-f0-9]+)")
-regex=re.compile("^ [0-9]")
-iregex=re.compile("([0-9]+) insert")
-dregex=re.compile("([0-9]+) delet")
-for line in sys.stdin:
-		
-	c=cregex.match(line)
-	if c:
-		commits+=1
-		if display_sha:
-			print c.group(1)
-	if regex.match(line):
-		i=get_count(iregex,line)
-		insertions+=i
-		d=get_count(dregex,line)
-		deletions+=d
-
-print "Commits: {0}".format(commits)
-print "Insertions: {0}".format(insertions)
-print "Deletions: {0}".format(deletions)
+print "Last commit processed: {0}".format(stats.last)
+print "Commits: {0}".format(stats.commits)
+print "Insertions: {0}".format(stats.insertions)
+print "Deletions: {0}".format(stats.deletions)
